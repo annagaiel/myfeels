77a3d6b1001622edf1737352c46c407d6829b464
annagaiel <annagaiel@gmail.com>
2015-12-01 23:42:56 -0800

    Change the blockquote style to the user's choice

--- app/assets/javascripts/phrase.js
+++ app/assets/javascripts/phrase.js
@@ -1,3 +1,21 @@
 $(document).on('click', '#preview-button', function(){
-  $('#preview-section').fadeToggle();
+  $('#preview-section').toggle();
+
+  var phrase = $('#phrase_phrase').val();
+  var author = $('#phrase_author').val();
+  var bgColor = $('#phrase_background').val();
+  var fgColor = $('#phrase_foreground').val();
+
+  $('#preview-section .quote-text').text(phrase);
+  $('#preview-section .phrase-bottom').text(author);
+
+  $('#preview-section blockquote').css({
+      "background-color": bgColor,
+      "color": fgColor
+  });
+
+  $('hr').css({
+      "border-top": "1px solid " + fgColor
+  });
+
 });
